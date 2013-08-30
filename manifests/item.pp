f95fed52785715061a8a451faf445d983846501a
Ian Oswald <ian@ozzie.io>
2013-08-30 18:13:08 +1000

    Improved readability of commands by replacing excaped double quotes with single quotes.

--- manifests/item.pp
+++ manifests/item.pp
@@ -19,19 +19,19 @@ define dockutil::item (
   case $ensure {
     'present': {
       $run = $position ? {
-        'unset' => "${boxen::config::cachedir}/dockutil/scripts/dockutil --add \"${item}\" --label \"${name}\" --no-restart",
-        default => "${boxen::config::cachedir}/dockutil/scripts/dockutil --add \"${item}\" --label \"${name}\" --position ${position} --no-restart",
+        'unset' => "${boxen::config::cachedir}/dockutil/scripts/dockutil --add '${item}' --label '${name}' --no-restart",
+        default => "${boxen::config::cachedir}/dockutil/scripts/dockutil --add '${item}' --label '${name}' --position ${position} --no-restart",
       }
       exec {"dockutil-add-${name}":
         command => $run,
-        onlyif  => "${boxen::config::cachedir}/dockutil/scripts/dockutil --find \"${name}\" | grep -qx \"${name} was not found in /Users/${::luser}/Library/Preferences/com.apple.dock.plist\"";
+        onlyif  => "${boxen::config::cachedir}/dockutil/scripts/dockutil --find '${name}' | grep -qx '${name} was not found in /Users/${::luser}/Library/Preferences/com.apple.dock.plist'";
       }
     }
 
     'absent':{
-      exec {"dockutil-remove-${name}":
-        command => "${boxen::config::cachedir}/dockutil/scripts/dockutil --remove \"${name}\" --no-restart",
-        onlyif  => "${boxen::config::cachedir}/dockutil/scripts/dockutil --find \"${name}\" | grep -q \"${name} was found\"";
+      exec { "dockutil-remove-${name}":
+        command => "${boxen::config::cachedir}/dockutil/scripts/dockutil --remove '${name}' --no-restart",
+        onlyif  => "${boxen::config::cachedir}/dockutil/scripts/dockutil --find '${name}' | grep -q '${name} was found'";
       }
     }
   }
